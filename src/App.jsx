923d42c9ccb19fa22a120f71cee5ab3a4a821831
Stanislav2022 <tsv5@ukr.net>
2023-06-15 09:58:09 +0300

    services

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import css from "./App.module.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import AboutUs from "./components/AboutUs/AboutUs";
+import OurServices from "components/OurServices/OurServices";
 import Futer from "./components/Futer/Futer";
 import PriceList from "./components/PriceList/PriceList";
 
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Hero />
         <AboutUs />
+        <OurServices/>
         <PriceList />
       </main>
       <Futer />
