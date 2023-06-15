6dc4952ce081edd914b5b5bd616d67cc254af9c4
Stanislav2022 <tsv5@ukr.net>
2023-06-15 17:49:33 +0300

    logo

--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -27,39 +27,40 @@ const PriceList = () => {
 
   return (
     <div className={css.price__section}>
-            <div className={css.title__wrapper}>
+      <div className={css.title}>
         <h2>Розрахунок ціни</h2>
         <FcMinus size="2em"/>
       </div>
-      <div className={css.wrapper}>
-        <h3>Організаційно-правова форма</h3>
-        <Button
-          selected={"IE" === serviceSelect.section}
-          onClick={() => setSection("IE")}
-        >
-          ФОП  <MdBusinessCenter size="18px"/>
-        </Button>
-        <Button
-          selected={"LLC" === serviceSelect.section}
-          onClick={() => setSection("LLC")}
-           > ТОВ  <MdBusiness size="18px"/> 
-      </Button>
-      </div>
-      <div className={css.wrapper}>
-        <h3>Режим оподаткування</h3>
-        <Button
-            selected={"single tax" === serviceSelect.taxation}
-            onClick={() => setTaxation("single tax")}
+      <div>
+        <div className={css.wrapper}>
+          <h3>Організаційно-правова форма</h3>
+          <Button
+            selected={"IE" === serviceSelect.section}
+            onClick={() => setSection("IE")}
           >
-            Єдиний податок
+            ФОП  <MdBusinessCenter size="18px"/>
+          </Button>
+          <Button
+            selected={"LLC" === serviceSelect.section}
+            onClick={() => setSection("LLC")}
+            > ТОВ  <MdBusiness size="18px"/> 
         </Button>
-        <Button
-            selected={"general" === serviceSelect.taxation}
-            onClick={() => setTaxation("general")}
-             >
-            Загальна система
-      </Button>
-      <div className={css.wrapper}>
+        </div>
+        <div className={css.wrapper}>
+          <h3>Режим оподаткування</h3>
+          <Button
+              selected={"single tax" === serviceSelect.taxation}
+              onClick={() => setTaxation("single tax")}
+            >
+              Єдиний податок
+          </Button>
+          <Button
+              selected={"general" === serviceSelect.taxation}
+              onClick={() => setTaxation("general")}
+              >
+              Загальна система
+          </Button>
+          <div className={css.wrapper}>
             <input
               type="checkbox"
               className={css.checkbox}
@@ -67,30 +68,31 @@ const PriceList = () => {
               onChange={() => setVat(!serviceSelect.vat)}
             />
             <p className={css.text}>Платник ПДВ</p>
-      </div>
-      </div>
-      <div className={css.wrapper}>
-        <h3>Кількість найманих працівників: {employee}</h3>
-        <input type="range" min="0" max="10" onChange={(event) => setEmployee(event.target.value)} list="markers" defaultValue="0"/>
-        <datalist id="markers">
-          <option value="0"></option>
-          <option value="1"></option>
-          <option value="2"></option>
-          <option value="3"></option>
-          <option value="4"></option>
-          <option value="5"></option>
-          <option value="6"></option>
-          <option value="7"></option>
-          <option value="8"></option>
-          <option value="9"></option>       
-          <option value="10"></option>
-        </datalist>
-      </div>
-      <div className={css.wrapper}>
-        <h3>Загальна сума:</h3>
-        <p className={css.amount}> {totalAmount} грн/міс.</p>
-      </div>
-      <div className={css.wrapper}>        <p>*Ціни орієнтовні і складаються з розрахунку 350 грн/годину роботи. Кінцева ціна пакету обговорюється індивідуально.</p></div>
+          </div>
+        </div>
+        <div className={css.wrapper}>
+          <h3>Кількість найманих працівників: {employee}</h3>
+          <input type="range" min="0" max="10" onChange={(event) => setEmployee(event.target.value)} list="markers" defaultValue="0"/>
+          <datalist id="markers">
+            <option value="0"></option>
+            <option value="1"></option>
+            <option value="2"></option>
+            <option value="3"></option>
+            <option value="4"></option>
+            <option value="5"></option>
+            <option value="6"></option>
+            <option value="7"></option>
+            <option value="8"></option>
+            <option value="9"></option>       
+            <option value="10"></option>
+          </datalist>
+        </div>
+        <div className={css.wrapper}>
+          <h3>Загальна сума:</h3>
+          <p className={css.amount}> {totalAmount} грн/міс.</p>
+        </div>  
+      </div>  
+      <p>*Ціни орієнтовні і складаються з розрахунку 350 грн/годину роботи. Кінцева ціна пакету обговорюється індивідуально.</p>
 
     </div>
   );
